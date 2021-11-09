b4bbc8bd613f23b59dcc039ee360003bc30a0fdf
komppAtte <atte.v.komppa@student.jyu.fi>
2021-11-09 15:27:36 +0200

    tehtävät tehty 4.19 asti

--- Osa4/app.js
+++ Osa4/app.js
@@ -6,6 +6,7 @@ const config = require('./utils/config')
 const logger = require('./utils/logger')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const mongoose = require('mongoose')
 
 logger.info('Connecting: ', config.MONGODB_URI)
@@ -24,5 +25,6 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 
   app.use('/api/blogs', blogsRouter)
   app.use('/api/users', usersRouter)
+  app.use('/api/login', loginRouter)
   
   module.exports = app
