68c96b661746b38de792db5b79bed7b6bb1676a3
komppAtte <atte.v.komppa@student.jyu.fi>
2021-11-10 10:33:47 +0200

    tehtävät 4.20 - 4.21

--- Osa4/controllers/blogs.js
+++ Osa4/controllers/blogs.js
@@ -5,27 +5,18 @@ const Blog = require('../models/blog')
 const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
-const getTokenFrom = request => {
-  const authorization = request.get('authorization')
-  if (authorization && authorization.toLowerCase().startsWith('bearer ')) {
-    return authorization.substring(7)
-  }
-  return null
-}
-
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs.map(blog => blog.toJSON()))
 })
 
-blogsRouter.post('/', async (request, response) => {
+blogsRouter.post( '/', async (request, response ) => {
   const body = request.body
-  const token = getTokenFrom(request)
-  const decodedToken = jwt.verify(token, process.env.SECRET)
-  if (!token || !decodedToken.id) {
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if ( !decodedToken.id ) {
     return response.status(401).json({ error: 'invalid token' })
   }
-  const user = await User.findById(decodedToken.id)
+  const user = await User.findById( decodedToken.id )
 
   const blog = new Blog({
     title: body.title,
@@ -43,7 +34,7 @@ blogsRouter.post('/', async (request, response) => {
   }
 })
 
-blogsRouter.get("/:id", async (request, response) => {
+blogsRouter.get( "/:id", async ( request, response ) => {
   const blog = await Blog.findById(request.params.id)
   if ( blog ) {
     response.json(blog.toJSON())
@@ -53,11 +44,25 @@ blogsRouter.get("/:id", async (request, response) => {
 })
 
 blogsRouter.delete("/:id", async (request, response) => {
-  await Blog.findByIdAndRemove(request.params.id)
-  response.status(204).end()
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+
+  if( !decodedToken.id ) {
+    return response.status(401).json({ error: 'invalid token' } )
+  }
+
+  const userId = decodedToken.id
+  const blog = await Blog.findById(request.params.id)
+  console.log(blog)
+
+  if ( blog.user.toString() === userId.toString() ) {
+    await Blog.findByIdAndDelete(request.params.id)
+    response.status(204).end()
+  } else {
+    return response.status(401).json({ error: 'invalid token, can`t delete others blogs'})
+  }
 })
 
-blogsRouter.put('/:id', (request, response, next) => {
+blogsRouter.put( '/:id', (request, response, next ) => {
   const body = request.body
 
   const blog = {
